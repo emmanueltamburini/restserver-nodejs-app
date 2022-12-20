4b64e1e2e5ffe7e60e9baf28c3f6085dcfb5e167
Emmanuel Tamburini <emmanueltamburini@gmail.com>
2022-12-20 09:45:01 -0400

    delete user enpoint (changing status) has done

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -66,7 +66,7 @@ export const userPut = async (req = request, res = response) => {
 export const userDelete = async (req = request, res = response) => {
   const { id } = req.params;
 
-  const user = await User.findByIdAndDelete(id);
+  const user = await User.findByIdAndUpdate(id, {status: false}, {new: true});
 
   res.json({
     user,
